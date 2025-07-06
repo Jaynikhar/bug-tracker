110932c00434cf7caeceeeabf1c4133413263174
Jaynikhar <jaynikhar61@gmail.com>
2025-07-06 10:44:35 +0530

    Delete client/src directory

--- client/src/services/api..js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
-
-export const login = (data) => API.post("/auth/login", data);
-export const register = (data) => API.post("/auth/register", data);
-export const createBug = (data, token) => API.post("/bugs/create", data, { headers: { Authorization: `Bearer ${token}` } });
-export const getMyBugs = (token) => API.get("/bugs/mybugs", { headers: { Authorization: `Bearer ${token}` } });
